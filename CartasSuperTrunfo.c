c9e21dfc85e59ff9d2a97efc2b2c8813afb2d95a
YureOliver <yure_elmask@hotmail.com>
2025-02-23 22:17:35 +0000

    SUPERTRUNFO AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -41,6 +41,13 @@ int main(){
     scanf("%d", &pturistico1);
     printf("CARTA CADASTRADA!\n\n");
 
+     //CALCULOS DE DENSIDADE POPULACIONAL E PIB PER CAPITA
+
+     float dpopulacional = populacao/area;
+     float dpopulacional1 = populacao1/area1;
+     float pibpc = pib/populacao;
+     float pibpc1 = pib1/populacao1;
+
     //SAIDA DE DADOS CARTA 1
 
     printf(" =§=§=§=§=§= CARTA 1 =§=§=§=§=§= \n\n");
@@ -51,17 +58,21 @@ int main(){
     printf("ÁREA: %f KM²\n", area);
     printf("PIB: %f BILHÕES DE REAIS\n", pib);
     printf("PONTOS TURÍSTICOS: %d\n", pturistico);
+    printf("A DENSIDADE POPULACIONAL É: %f HAB/KM²\n", dpopulacional);
+    printf("O PIB PER CAPITA É: %f REAIS\n", pibpc);
 
     //SAIDA DE DADOS CARTA 2
     
     printf(" \n\n=§=§=§=§=§= CARTA 2 =§=§=§=§=§= \n\n");
     printf("ESTADO: A\n");
-    printf("CODIGO: A01\n");
+    printf("CODIGO: A02\n");
     printf("NOME DA CIDADE: %s\n", cidade1);
     printf("POPULAÇÃO: %d\n", populacao1);
     printf("ÁREA: %f KM²\n", area1);
     printf("PIB: %f BILHÕES DE REAIS\n", pib1);
     printf("PONTOS TURÍSTICOS: %d\n", pturistico1);
+    printf("A DENSIDADE POPULACIONAL É: %f HAB/KM²\n", dpopulacional1);
+    printf("O PIB PER CAPITA É: %f REAIS\n", pibpc1);
 
 
     return 0;
